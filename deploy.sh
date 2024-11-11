5da7112a7df29d29d56bb0f7e387e510bfe124c6
bingis-khan <robert.noparlik@gmail.com>
2024-11-11 20:41:03 +0100

    scp -> rsync, because we want to replace directories + bugfix with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dotnet publish
-scp -r "bin/Release/net8.0/publish" "$SERVER_URL":/usr/local/bin/csrss
+rsync -r "bin/Release/net8.0/publish" "$SERVER_URL":/usr/local/bin/csrss  # remember to install rsync in the remote server also.
 ssh "$SERVER_URL" rc-service csrss restart
 
